806d0841ebdeedd9699dff9aebe4531e7a41381e
Nada Mohamed <nado2000avvwad@gmail.com>
2022-12-21 22:49:28 +0200

    first commit

---
+++ app/src/main/java/com/example/online_shopping/MenActivity.java
@@ -0,0 +1,14 @@
+package com.example.online_shopping;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_men);
+    }
+}
---
+++ app/src/main/java/com/example/online_shopping/submitActivity.java
@@ -0,0 +1,18 @@
+package com.example.online_shopping;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class submitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submit);
+
+    }
+
+
+}
